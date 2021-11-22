040c0eb54c3f9b28f447a365d8e24351025c4395
motohirotakagi <tkg.8.mthr@gmail.com>
2021-11-22 17:53:57 +0900

    Reducerの作成

--- store/index.js
+++ store/index.js
@@ -1,5 +1,17 @@
 import { createStore } from "redux";
 
+const initialState = {
+  count: 1
+}
+
+// storeを作成するためにreducerを定義
+// reducerは引数にstateを持つ
+
+const reducer = (state = inirialState) => {
+  return state;
+}
+
+// reducerが保持しているstateをstoreの中に保存する
 const store = createStore();
 
 export default store;
